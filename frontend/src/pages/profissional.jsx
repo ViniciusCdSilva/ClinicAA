51237fa236553bf461c3f1bcd8ec5f9539f13c56
Vinicius Silva <viniciuscassianosilva13@gmail.com>
2026-04-18 19:52:22 -0300

    pagina paciente

--- frontend/src/pages/profissional.jsx
+++ frontend/src/pages/profissional.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './profissional.css';
 
 export default function Profissional() {
   return (
     <div>
-      <h1>Página do Profissional</h1>
+    <div className='areapes'>
+      <input className = "pesquisa" type="text" placeholder='Pesquisar' onChange={handleChange}  />
+      <p><Strong>Pesquise</Strong><br />O nome do paciente ou estagiario para ver os detalhes </p>
     </div>
+       <div className='mostrar estagio'></div>
+       <div className='mostrar paciente'></div>
+       <div className='mostrar consultas'></div>
+    </div>
+
+    
   );
 }
